d39f9f4c15e23c499fab338c64679d8c8bff59ba
vcu-stricklercb <78453263+vcu-stricklercb@users.noreply.github.com>
2021-05-04 19:05:30 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,16 @@
 def spiralize(size, n=1):
     """ Your code goes somewhere in here"""
+    n = 6
+    tally = 0
+    increment = 2
+    total = 0
+    
+    while n <= number ** 2:
+        total += n
+        n += increment
+        tally += 1
+        if tally == 4:
+            incrt += 2
+            tally = 0
     return_value = n
     return return_value
